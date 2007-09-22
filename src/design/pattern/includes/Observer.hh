afac6973e06a3cac1e44412b51578ba848b9dfd1
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2007-09-22 16:11:42 +0100

    added observer pattern templates

---
+++ src/design/pattern/includes/Observer.hh
@@ -0,0 +1,59 @@
+// Copyright (C) 2007 Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+//
+// This file is free software; as a special exception the author gives
+// unlimited permission to copy and/or distribute it, with or without
+// modifications, as long as this notice is preserved.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#ifndef OBSERVER_HH
+#define OBSERVER_HH
+
+
+namespace des
+{
+    namespace design
+    {
+
+
+/**
+ * This is is a template to declare the observer of the GOF's observer pattern.
+ * Usage:
+ * <code>
+ *   class ConcreteObserver : public Observer<ConcreteSubject> {}
+ * </code>
+ * and implement the <code>update()</code> method in order to act upon the state
+ * change.
+ *
+ * This template was mainly taken from
+ * <a href="http://www.codeproject.com/cpp/observer_with_templates.asp" />.
+ *
+ * @author <a href="mailto:Dominik.Dahlem@cs.tcd.ie">Dominik Dahlem</a>
+ */
+template <class T>
+class Observer
+{
+public:
+    Observer()
+        {}
+
+    virtual ~Observer()
+        {}
+
+    /**
+     * Update the internal state of the observer based on the state change of
+     * the subject. The subject is passed as a parameter into this method in
+     * order to be able to call back on the specific change.
+     *
+     * @param T* the subject that notified the concrete observer.
+     */
+    virtual void update(T *subject) = 0;
+};
+
+
+    }
+}
+
+#endif
---
+++ src/design/pattern/includes/Subject.hh
@@ -0,0 +1,79 @@
+// Copyright (C) 2007 Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+//
+// This file is free software; as a special exception the author gives
+// unlimited permission to copy and/or distribute it, with or without
+// modifications, as long as this notice is preserved.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#ifndef SUBJECT_HH
+#define SUBJECT_HH
+
+
+#include <vector>
+using std::vector;
+
+
+
+namespace des
+{
+    namespace design
+    {
+
+
+/**
+ * This is is a template to declare the subject of the GOF's observer pattern.
+ * Usage:
+ * <code>
+ *   class ConcreteSubject: public Subject<ConcreteSubject> {}
+ * </code>
+ * and implement a state change method that calls <code>notify()</code>. Also,
+ * implement a method that allows the concrete observer to ask for the specific
+ * state change.
+ * This template was mainly taken from
+ * <a href="http://www.codeproject.com/cpp/observer_with_templates.asp" />.
+ *
+ * @author <a href="mailto:Dominik.Dahlem@cs.tcd.ie">Dominik Dahlem</a>
+ */
+template <class T>
+class Subject
+{
+public:
+    Subject()
+        {}
+
+    virtual ~Subject()
+        {}
+
+    /**
+     * Attach an observer to a subject.
+     *
+     * @param Observer<T> the observer to be attached to the subject
+     */
+    void attach (Observer<T> &observer)
+        {
+            m_observers.push_back(&observer);
+        }
+
+    /**
+     * Notify the attached observers.
+     */
+    void notify ()
+        {
+            vector<Observer<T> *>::iterator it;
+
+            for (it=m_observers.begin();it!=m_observers.end();it++) {
+                (*it)->update(static_cast<T *>(this));
+            }
+        }
+
+private:
+    vector<Observer<T> *> m_observers;
+};
+
+    }
+}
+
+#endif
