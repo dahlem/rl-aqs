2e3cb33ac24488cffdbc52713faf60b8e7ce2dbb
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2008-01-29 19:27:13 +0000

    Implemented LHS sampling.

---
+++ octave/dd_sampling/inst/lhs.m
@@ -0,0 +1,37 @@
+## Copyright (C) 2008 Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+##  
+## This file is free software; as a special exception the author gives
+## unlimited permission to copy and/or distribute it, with or without 
+## modifications, as long as this notice is preserved.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+## implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+## Author: Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+## Maintainer: Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+## Keywords: Latin Hypercube Sampling
+## Created: 29.01.2008
+## Version: 0.1
+
+## S = lhs(n, min, max)
+## uniform Latin Hypercube Sampling (from matlab)
+## Input:
+##  n   : number of samples
+##  min : vector (x_1, x_2, ..., x_k) of the minimum values for each dimension
+##  max : vector (x_1, x_2, ..., x_k) of the maximum values for each dimension
+function S = lhs(n, min, max)
+  nvar = length(min);
+  u = rand(n, nvar);
+  S = zeros(n, nvar);
+
+  for j=1: nvar
+    # generate the random permutation of the integers 1...n
+    idx = randperm(n);
+
+    P = (idx' - u(:,j)) / n;
+
+    # scale the random variable to [min, max]
+    S(:,j) = min(j) + P .* (max(j) - min(j));
+  end
+endfunction
