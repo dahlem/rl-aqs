4839c58d1b4b32e6a4417f1039c8760c0322b501
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2008-06-19 17:16:43 +0100

    Documented the code.

--- src/network/evolution/WEvonet.cc
+++ src/network/evolution/WEvonet.cc
@@ -13,6 +13,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+/** @file WEvonet.cc
+ * Implementation of the methods declared in WEvonet.hh.
+ *
+ * @author Dominik Dahlem
+ */
 #include <fstream>
 #include <numeric>
 #include <vector>
--- src/network/evolution/includes/WEvonet.hh
+++ src/network/evolution/includes/WEvonet.hh
@@ -14,6 +14,11 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+/** @file WEvonet.hh
+ * Declaration of the methods for the weighted directed network generation model.
+ *
+ * @author Dominik Dahlem
+ */
 #ifndef __WEVONET_HH__
 #define __WEVONET_HH__
 
@@ -39,6 +44,9 @@ using boost::graph_property_iter_range;
 #include <gsl/gsl_rng.h>
 
 
+/** @enum vertex_service_rate_t
+ * This enum extends the vertex properties by a service_rate attribute.
+ */
 enum vertex_service_rate_t { vertex_service_rate = 1111 };
 
 
@@ -53,43 +61,140 @@ namespace des
     namespace network
     {
 
+/** @typedef VertexServiceRateProperty
+ * Specifies the property for the vertex service rate
+ */
 typedef property<vertex_service_rate_t, float> VertexServiceRateProperty;
+
+/** @typedef EdgeWeightProperty
+ * Specifies the property for the edge weight
+ */
 typedef property<edge_weight_t, float> EdgeWeightProperty;
 
-typedef adjacency_list<setS,    // no parallel edges; most expensive space complexity
-                       listS,   // add/remove verteces quickly; constant time complexity
+/** @typedef Graph
+ * Specifies the Graph as an adjacency list. The edges are represented in a
+ * set which means that no parallel edges are allowed. However, of the available
+ * STL containers, this is the most expensive in terms of space complexity. The Vertices
+ * are stored in a STL list to allow quick addition and removal with constant time
+ * complexity.
+ * @todo ddahlem: verify whether bidirectionality is required.
+ */
+typedef adjacency_list<setS,
+                       listS,
                        bidirectionalS,
                        property<vertex_index_t, uint32_t, VertexServiceRateProperty>,
                        EdgeWeightProperty> Graph;
 
+/** @typedef Vertex
+ * Specifies the vertex descriptor of a graph
+ */
 typedef graph_traits<Graph>::vertex_descriptor Vertex;
+
+/** @typedef Edge
+ * Specifies the edge descriptor of a graph
+ */
 typedef graph_traits<Graph>::edge_descriptor Edge;
+
+/** @typedef VertexServiceRateMap
+ * Specifies the map that stores the vertex service rate property
+ */
 typedef property_map<Graph, vertex_service_rate_t>::type VertexServiceRateMap;
+
+/** @typedef VertexIndexMap
+ * Specifies the map that stores the vertex indeces
+ */
 typedef property_map<Graph, vertex_index_t>::type VertexIndexMap;
+
+/** @typedef EdgeWeightMap
+ * Specifies the edge weight property
+ */
 typedef property_map<Graph, edge_weight_t>::type EdgeWeightMap;
 
+/** @typedef VServiceIterator
+ * Specifies the iterator for the vertex service rates
+ */
 typedef graph_property_iter_range<Graph, vertex_service_rate_t>::iterator VServiceIterator;
+
+/** @typedef OutEdgeIterator
+ * Specifies the iterator for the out degree edges
+ */
 typedef graph_traits<Graph>::out_edge_iterator OutEdgeIterator;
 
+/** @typedef tGraphSP
+ * Specifies shared pointer to the Graph object
+ */
 typedef shared_ptr<Graph> tGraphSP;
+
+/** @typedef tGslRngSP
+ * Specifies shared pointer to the GSL random number generator objects
+ */
 typedef shared_ptr<gsl_rng> tGslRngSP;
 
 
+
+/** @class WEvonet
+ * This class specifies the interface to the weighted directed graph evolution model.
+ * The model implemented is detailed in a paper published to the Physical Review Letters
+ * by Alain Barrat titled "Weighted Evolving Networks: Coupling Topology and Weight Dynamics.
+ */
 class WEvonet
 {
 public:
+    /**
+     * Constructor which evolves a weighted directed network given a particular size.
+     *
+     * @param int the size of the network
+     * @param tGslRngSP the GSL random number to draw the number of edges a new
+     *        vertex is linked up to
+     * @param tGslRngSP the GSL random number to draw a uniform number to decide
+     *        which vertex to link up to.
+     */
     WEvonet(int p_size, tGslRngSP p_edge_rng, tGslRngSP p_uniform_rng);
+
+    /**
+     * Destructor
+     */
     ~WEvonet();
 
+    /** @fn void advance(int p_steps)
+     * Evolve the network for a further given steps
+     *
+     * @param int the number of iterations (i.e., vertices to add).
+     */
     void advance(int p_steps);
+
+    /** @fn void print(const std::string& filename)
+     * Print the graph using the graphviz interface of BGL into a file
+     *
+     * @param const std::string& the filename to be printed into
+     */
     void print(const std::string& filename);
 
 private:
+
+    /** @fn void assign_edge_weights(Vertex &v)
+     * Assign the edge weights to the created edges of the newly created
+     * vertex.
+     *
+     * @param Vertex& The newly created vertex
+     */
     void assign_edge_weights(Vertex &v);
 
+    /**
+     * The random number generator to determine how many edges should be
+     * created originating from the newly created vertex.
+     */
     tGslRngSP num_edges_rng;
+
+    /**
+     * The random number generator to determine a uniform random number in the range
+     * [0, 1) to decide which vertices to link up to.
+     */
     tGslRngSP uniform_rng;
 
+    /**
+     * The BGL graph object
+     */
     tGraphSP g;
 };
 
