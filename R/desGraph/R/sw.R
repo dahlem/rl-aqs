c3172c5f2bd2e83e0b0543179d8efdd978d16c6d
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2008-07-21 15:15:12 +0100

    Added a graph with 50000 nodes; Extended small-world analysis.

--- R/desGraph/R/sw.R
+++ R/desGraph/R/sw.R
@@ -18,6 +18,7 @@ des.graph.sw <- function(graph) {
   diameter <- diameter(graph)
   order <- log(vcount(graph))
   cluster.coefficient <- des.graph.cluster(graph)
+  edge.density <- graph.density(graph)
 
   return(data.frame(average.path,
                     diameter,
@@ -28,11 +29,11 @@ des.graph.sw <- function(graph) {
 
 des.graph.cluster.local <- function(graph) {
   cs <- rep(NaN, vcount(graph))
-  
+
   for (v in V(graph)) {
     # calculate the local neighbourhoods
     vertices <- neighborhood.size(graph, 1, v, mode="out")
-    
+
     # calculate the total in+out degree of a given vertex
     if (vertices > 1) {
       e <- vertices - 1
@@ -48,4 +49,62 @@ des.graph.cluster.local <- function(graph) {
 
 des.graph.cluster <- function(graph) {
   return(mean(des.graph.cluster.local(graph), na.rm=TRUE))
+}
+
+
+des.graph.trans.smallworldness <- function(graphSW, graphER=NULL) {
+  if (is.null(graphER)) {
+    graphER <- erdos.renyi.game(vcount(graphSW), ecount(graphSW),
+                                type="gnm", directed=TRUE)
+  }
+
+  return(des.graph.trans.ratio(graphSW, graphER) /
+         des.graph.path.ratio(graphSW, graphER))
+}
+
+
+des.graph.trans.ratio <- function(graphSW, graphER) {
+  return(transitivity(graphSW, type="global")
+         / transitivity(graphER, type="global"))
+}
+
+
+des.graph.path.ratio <- function(graphSW, graphER) {
+  return(average.path.length(graphSW) / average.path.length(graphER))
+}
+
+
+des.graph.cluster.ratio <- function(graphSW, graphER) {
+  return(des.graph.cluster(graphSW) / des.graph.cluster(graphER))
+}
+
+
+des.graph.cluster.smallworldness <- function(graphSW, graphER=NULL) {
+  if (is.null(graphER)) {
+    graphER <- erdos.renyi.game(vcount(graphSW), ecount(graphSW),
+                                type="gnm", directed=TRUE)
+  }
+
+  return(des.graph.cluster.ratio(graphSW, graphER) /
+         des.graph.path.ratio(graphSW, graphER))
+}
+
+
+des.graph.smallworld.equiv <- function(p, graphSW, graphER) {
+  return(des.graph.trans.ratio(graphSW, graphER) -
+         des.graph.wattsstrogatz.smallworldness(mean(degree(graphSW, mode="total")) / 2,
+                                                vcount(graphSW),
+                                                p))
+}
+
+
+des.graph.wattsstrogatz.smallworldness <- function(K, N, p) {
+  return((log(N) / (log(2 * p * K) + log(N))) * N *
+         ((p * 3 * (K - 1) * (1 - p)^3) /  (log(2 * K) * (2 * K - 1))))
+}
+
+
+des.graph.smallworld.optim <- function(graphSW, graphER) {
+  uniroot(des.graph.smallworld.equiv, c(0.1,0.99),
+          tol=0.0001, graphSW=graphSW, graphER=graphER)
 }
