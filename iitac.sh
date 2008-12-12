13243815da66f4c9d87bc1a1eb1f7f1df8ace174
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2008-12-12 17:30:18 +0000

    added configure script for iitac

---
+++ iitac.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/usr/support/arch/x86_64/BOOST/1.37.0/lib:$LD_LIBRARY_PATH
+
+./configure LDFLAGS=-L/usr/support/arch/x86_64/BOOST/1.37.0/lib -L/usr/support/arch/x86_64/GSL/gsl-1.9-gcc-3.4.6/lib CPPFLAGS=-I/usr/support/arch/x86_64/GSL/gsl-1.9-gcc-3.4.6/include -I/usr/support/arch/x86_64/BOOST/1.37.0/include/boost-1_37 -with-boost-libdir=/usr/support/arch/x86_64/BOOST/1.37.0/lib --with-boost=/usr/support/arch/x86_64/BOOST/1.37.0 --with-gsl-prefix=/usr/support/arch/x86_64/GSL/gsl-1.9-gcc-3.4.6 --enable-mpi
