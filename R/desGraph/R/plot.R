1968ab74cc7ace020d4aa82772af5885d2b7fbdd
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2009-01-03 12:52:14 +0000

    Finished the plots for the simulation output.

--- R/desGraph/R/plot.R
+++ R/desGraph/R/plot.R
@@ -43,24 +43,33 @@ des.graph.arrival.plot <- function(graph, filename, ps=TRUE) {
   }
 }
 
-des.graph.plots <- function(simEndGraph, graphs, prefix, stopTime) {
-  graph = read.graph(simEndGraph, format="graphml")
-  
-  des.graph.arrival.hist.plot(graph)
-  des.graph.utilisation.plot(graph)
-  des.graph.utilisation.evo.plot(prefix, graphs, stopTime)
-  des.graph.average.delay.in.queue.plot(graph)
-  des.graph.average.delay.in.queue.evo.plot(prefix, graphs, stopTime)
+
+des.graph.sim.main <- function(replications, graphs, nodes, stopTime) {
+  des.graph.arrival.hist.plot(replications, graphs, nodes)
+  des.graph.utilisation.plot(replications, graphs, nodes)
+  des.graph.utilisation.evo.plot(replications, graphs, nodes, stopTime)
+  des.graph.utilisation.customers.evo.plot(replications, graphs, nodes, stopTime)
+  des.graph.delay.customers.evo.plot(replications, graphs, nodes, stopTime)
+  des.graph.average.delay.in.queue.plot(replications, graphs, nodes)
+  des.graph.average.delay.in.queue.evo.plot(replications, graphs, nodes, stopTime)
 }
 
-des.graph.arrival.hist.plot <- function(graph, ps=TRUE) {
+des.graph.arrival.hist.plot <- function(replications, graphs, nodes, ps=TRUE) {
   if (ps) {
     postscript("graph-arrival-hist-plot.eps", onefile=FALSE)
   }
 
-  arrivalT <- get.vertex.attribute(graph, "arrival_rate");
-  meanT <- mean(arrivalT)
-  df <- data.frame(arrivalTimes=arrivalT)
+  df <- data.frame(arrivalTimes=rep(0, nodes * replications));
+  
+  for (r in seq(1, replications)) {
+    print(paste("Read graph: ", r, "/graphs/graph0.gml", sep=""))
+    simEndGraph = paste(r, "/graphs/graph0.gml", sep="")
+    graph = read.graph(simEndGraph, format="graphml")
+    arrivalT <- get.vertex.attribute(graph, "arrival_rate");
+    df$arrivalTimes[(1 + (r - 1) * nodes):(nodes * r)] = arrivalT
+  }
+
+  meanT <- mean(df$arrivalTimes)
   
   p <- ggplot(df, aes(x=arrivalTimes)) 
   p <- p + geom_histogram(aes(y= ..density..))
@@ -78,14 +87,29 @@ des.graph.arrival.hist.plot <- function(graph, ps=TRUE) {
 }
 
 
-des.graph.utilisation.plot <- function(graph, ps=TRUE) {
+des.graph.utilisation.plot <- function(replications, graphs, nodes, ps=TRUE) {
   if (ps) {
     postscript("graph-utilisation-hist-plot.eps", onefile=FALSE)
   }
 
-  utilisations <- get.vertex.attribute(graph, "utilisation");
-  meanU <- mean(utilisations)
-  df <- data.frame(utilisation=utilisations)
+  files <- paste(seq(1, replications), "/graphs/graph", graphs - 1, ".gml", sep="")
+  fexists <- file.exists(files)
+  ftrue <- fexists[fexists == TRUE]
+  df <- data.frame(utilisation=rep(0, nodes * length(ftrue)));
+  counter <- 1
+  
+  for (r in seq(1, replications)) {
+    if (file.exists(paste(r, "/graphs/graph", graphs - 1, ".gml", sep="")) == TRUE) {
+      print(paste("Read graph: ", r, "/graphs/graph", graphs - 1, ".gml", sep=""))
+      simEndGraph = paste(r, "/graphs/graph", graphs - 1, ".gml", sep="")
+      graph = read.graph(simEndGraph, format="graphml")
+      utilisations <- get.vertex.attribute(graph, "utilisation");
+      df$utilisation[(1 + (counter - 1) * nodes):(nodes * counter)] <- utilisations
+      counter <- counter + 1
+    }
+  }
+
+  meanU <- mean(df$utilisation)
   
   p <- ggplot(df, aes(x=utilisation)) 
   p <- p + geom_histogram(aes(y= ..density..))
@@ -103,24 +127,45 @@ des.graph.utilisation.plot <- function(graph, ps=TRUE) {
 }
 
 
-des.graph.utilisation.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE) {
+des.graph.utilisation.evo.plot <- function(replications, graphs, nodes, stopTime, ps=TRUE) {
   if (ps) {
-    postscript("graph-utilisation-evo-plot.eps", onefile=FALSE)
+    pdf("graph-utilisation-evo-plot.pdf", onefile=FALSE, version=1.4)
   }
 
   interval <- stopTime / (graphs - 1);
-  df <- data.frame(time=rep(0, graphs), meanUtil=rep(0, graphs));
+  graphs <- graphs - 1
+  df <- data.frame(time=rep(0, graphs), meanUtil=rep(0, graphs), ucl=rep(0, graphs), lcl=rep(0, graphs));
 
-  df$time = seq(0, (graphs - 1)) * interval;
-  
-  for (i in seq(0, graphs - 1)) {
-    graph <- read.graph(paste(prefix, "/graph", i, ".gml", sep=""), format="graphml")
-    utilisations <- get.vertex.attribute(graph, "utilisation");
-    df$meanUtil[i + 1] <- mean(utilisations)
+  for (i in seq(1, graphs)) {
+    files <- paste(seq(1, replications), "/graphs/graph", i, ".gml", sep="")
+    fexists <- file.exists(files)
+    ftrue <- fexists[fexists == TRUE]
+    dflocal <- data.frame(utilisation=rep(0, nodes * length(ftrue)));
+    counter <- 1
+    
+    for (r in seq(1, replications)) {
+      if (file.exists(paste(r, "/graphs/graph", i, ".gml", sep="")) == TRUE) {
+        print(paste("Read graph: ", r, "/graphs/graph", i, ".gml", sep=""))
+        simEndGraph = paste(r, "/graphs/graph", i, ".gml", sep="")
+        graph = read.graph(simEndGraph, format="graphml")
+        utilisations <- get.vertex.attribute(graph, "utilisation");
+        dflocal$utilisation[(1 + (counter - 1) * nodes):(nodes * counter)] <- utilisations
+        counter <- counter + 1
+      }
+    }
+
+    ## calc mean and standard error
+    util.se <- sd(dflocal$utilisation) / sqrt(length(dflocal$utilisation))
+    df$meanUtil[i] <- mean(dflocal$utilisation)
+    df$ucl[i] <- df$meanUtil[i] + 1.96 * util.se
+    df$lcl[i] <- df$meanUtil[i] - 1.96 * util.se
   }
+
+  df$time = seq(1, graphs) * interval;
   
   p <- ggplot(df, aes(x=time, y=meanUtil))
   p <- p + layer(geom = "line")
+  p <- p + geom_smooth(aes(min=lcl, max=ucl), stat="identity")
   p <- p + scale_y_continuous("Mean Utilisation")
   p <- p + scale_x_continuous("Time")
   p <- p + opts(title="Evolution of Mean System Utilisation")
@@ -133,24 +178,44 @@ des.graph.utilisation.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE) {
 }
 
 
-des.graph.utilisation.customers.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE) {
+des.graph.utilisation.customers.evo.plot <- function(replications, graphs, nodes, stopTime, ps=TRUE) {
   if (ps) {
     postscript("graph-utilisation-vs-customers-evo-plot.eps", onefile=FALSE)
   }
 
   interval <- stopTime / (graphs - 1);
-  df <- data.frame(time=rep(0, graphs), meanUtil=rep(0, graphs), expectedAverageNumEvents=rep(0, graphs));
+  df <- data.frame(time=rep(0, graphs), meanUtil=rep(0, graphs),
+                   expectedAverageNumEvents=rep(0, graphs));
+
+  for (i in seq(0, graphs - 1)) {
+    files <- paste(seq(1, replications), "/graphs/graph", i, ".gml", sep="")
+    fexists <- file.exists(files)
+    ftrue <- fexists[fexists == TRUE]
+    dflocal <- data.frame(utilisation=rep(0, nodes * length(ftrue)),
+                          expectedAverageNumEvents=rep(0, nodes * length(ftrue)));
+    counter <- 1
+    
+    for (r in seq(1, replications)) {
+      if (file.exists(paste(r, "/graphs/graph", i, ".gml", sep="")) == TRUE) {
+        print(paste("Read graph: ", r, "/graphs/graph", i, ".gml", sep=""))
+        simEndGraph = paste(r, "/graphs/graph", i, ".gml", sep="")
+        graph = read.graph(simEndGraph, format="graphml")
+        utilisations <- get.vertex.attribute(graph, "utilisation")
+        expectedAverageNumEvents <- get.vertex.attribute(graph, "expected_average_number_event")
+        dflocal$utilisation[(1 + (counter - 1) * nodes):(nodes * counter)] <- utilisations
+        dflocal$expectedAverageNumEvents[(1 + (counter - 1) * nodes):(nodes * counter)] <- expectedAverageNumEvents
+        counter <- counter + 1
+      }
+    }
+
+    ## calc mean and standard error
+    util.se <- sd(dflocal$utilisation) / sqrt(length(dflocal$utilisation))
+    df$meanUtil[i+1] <- mean(dflocal$utilisation)
+    df$expectedAverageNumEvents[i+1] <- mean(dflocal$expectedAverageNumEvents)
+  }
 
   df$time = seq(0, (graphs - 1)) * interval;
-  
-  for (i in seq(0, graphs - 1)) {
-    graph <- read.graph(paste(prefix, "/graph", i, ".gml", sep=""), format="graphml")
-    utilisations <- get.vertex.attribute(graph, "utilisation");
-    expectAveNumEvents <- get.vertex.attribute(graph, "expected_average_number_event");
-    df$meanUtil[i + 1] <- mean(utilisations)
-    df$expectedAverageNumEvents[i + 1] <- mean(expectAveNumEvents)
-  }
-  
+
   p <- ggplot(df, aes(x=meanUtil, y=expectedAverageNumEvents))
   p <- p + layer(geom = "line")
   p <- p + scale_y_continuous("Expected Average Number of Events in Queue")
@@ -165,24 +230,43 @@ des.graph.utilisation.customers.evo.plot <- function(prefix, graphs, stopTime, p
 }
 
 
-des.graph.delay.customers.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE) {
+des.graph.delay.customers.evo.plot <- function(replications, graphs, nodes, stopTime, ps=TRUE) {
   if (ps) {
     postscript("graph-delay-vs-customers-evo-plot.eps", onefile=FALSE)
   }
 
   interval <- stopTime / (graphs - 1);
-  df <- data.frame(time=rep(0, graphs), meanUtil=rep(0, graphs), meanDelay=rep(0, graphs));
+  df <- data.frame(time=rep(0, graphs), meanDelay=rep(0, graphs),
+                   meanUtil=rep(0, graphs));
+
+  for (i in seq(0, graphs - 1)) {
+    files <- paste(seq(1, replications), "/graphs/graph", i, ".gml", sep="")
+    fexists <- file.exists(files)
+    ftrue <- fexists[fexists == TRUE]
+    dflocal <- data.frame(utilisation=rep(0, nodes * length(ftrue)),
+                          delay=rep(0, nodes * length(ftrue)));
+    counter <- 1
+    
+    for (r in seq(1, replications)) {
+      if (file.exists(paste(r, "/graphs/graph", i, ".gml", sep="")) == TRUE) {
+        print(paste("Read graph: ", r, "/graphs/graph", i, ".gml", sep=""))
+        simEndGraph = paste(r, "/graphs/graph", i, ".gml", sep="")
+        graph = read.graph(simEndGraph, format="graphml")
+        utilisations <- get.vertex.attribute(graph, "utilisation")
+        delay <- get.vertex.attribute(graph, "average_delay_in_queue")
+        dflocal$utilisation[(1 + (counter - 1) * nodes):(nodes * counter)] <- utilisations
+        dflocal$delay[(1 + (counter - 1) * nodes):(nodes * counter)] <- delay
+        counter <- counter + 1
+      }
+    }
+
+    ## calc mean and standard error
+    df$meanUtil[i+1] <- mean(dflocal$utilisation)
+    df$meanDelay[i+1] <- mean(dflocal$delay)
+  }
 
   df$time = seq(0, (graphs - 1)) * interval;
-  
-  for (i in seq(0, graphs - 1)) {
-    graph <- read.graph(paste(prefix, "/graph", i, ".gml", sep=""), format="graphml")
-    utilisations <- get.vertex.attribute(graph, "utilisation");
-    meanDelays <- get.vertex.attribute(graph, "average_delay_in_queue");
-    df$meanUtil[i + 1] <- mean(utilisations)
-    df$meanDelay[i + 1] <- mean(meanDelays)
-  }
-  
+
   p <- ggplot(df, aes(x=meanUtil, y=meanDelay))
   p <- p + layer(geom = "line")
   p <- p + scale_y_continuous("Mean Delay")
@@ -197,16 +281,31 @@ des.graph.delay.customers.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE
 }
 
 
-des.graph.average.delay.in.queue.plot <- function(graph, ps=TRUE) {
+des.graph.average.delay.in.queue.plot <- function(replications, graphs, nodes, ps=TRUE) {
   if (ps) {
     postscript("graph-average-delay-in-queue-hist-plot.eps", onefile=FALSE)
   }
 
-  averageDelayInQueue <- get.vertex.attribute(graph, "average_delay_in_queue");
-  meanD <- mean(averageDelayInQueue)
-  df <- data.frame(averageDelay=averageDelayInQueue)
+  files <- paste(seq(1, replications), "/graphs/graph", graphs - 1, ".gml", sep="")
+  fexists <- file.exists(files)
+  ftrue <- fexists[fexists == TRUE]
+  df <- data.frame(averageDelayInQueue=rep(0, nodes * length(ftrue)));
+  counter <- 1
   
-  p <- ggplot(df, aes(x=averageDelay)) 
+  for (r in seq(1, replications)) {
+    if (file.exists(paste(r, "/graphs/graph", graphs - 1, ".gml", sep="")) == TRUE) {
+      print(paste("Read graph: ", r, "/graphs/graph", graphs - 1, ".gml", sep=""))
+      simEndGraph = paste(r, "/graphs/graph", graphs - 1, ".gml", sep="")
+      graph = read.graph(simEndGraph, format="graphml")
+      averageDelayInQueue <- get.vertex.attribute(graph, "average_delay_in_queue");
+      df$averageDelayInQueue[(1 + (counter - 1) * nodes):(nodes * counter)] <- averageDelayInQueue
+      counter <- counter + 1
+    }
+  }
+
+  meanD <- mean(df$averageDelayInQueue)
+
+  p <- ggplot(df, aes(x=averageDelayInQueue)) 
   p <- p + geom_histogram(aes(y= ..density..))
   p <- p + geom_density(aes(y= ..density..), kernel="gaussian", adjust=2, fill=NA, colour="black")
   p <- p + geom_vline(intercept=meanD, colour="red")
@@ -222,24 +321,45 @@ des.graph.average.delay.in.queue.plot <- function(graph, ps=TRUE) {
 }
 
 
-des.graph.average.delay.in.queue.evo.plot <- function(prefix, graphs, stopTime, ps=TRUE) {
+des.graph.average.delay.in.queue.evo.plot <- function(replications, graphs, nodes, stopTime, ps=TRUE) {
   if (ps) {
-    postscript("graph-average-delay-in-queue-evo-plot.eps", onefile=FALSE)
+    pdf("graph-average-delay-in-queue-evo-plot.pdf", onefile=FALSE, version=1.4)
   }
 
   interval <- stopTime / (graphs - 1);
-  df <- data.frame(time=rep(0, graphs), averageDelay=rep(0, graphs));
+  graphs <- graphs - 1
+  df <- data.frame(time=rep(0, graphs), meanDelay=rep(0, graphs), ucl=rep(0, graphs), lcl=rep(0, graphs));
 
-  df$time = seq(0, (graphs - 1)) * interval;
-  
-  for (i in seq(0, graphs - 1)) {
-    graph <- read.graph(paste(prefix, "/graph", i, ".gml", sep=""), format="graphml")
-    averageDelay <- get.vertex.attribute(graph, "average_delay_in_queue");
-    df$averageDelay[i + 1] <- mean(averageDelay)
+  for (i in seq(1, graphs)) {
+    files <- paste(seq(1, replications), "/graphs/graph", i, ".gml", sep="")
+    fexists <- file.exists(files)
+    ftrue <- fexists[fexists == TRUE]
+    dflocal <- data.frame(delay=rep(0, nodes * length(ftrue)));
+    counter <- 1
+    
+    for (r in seq(1, replications)) {
+      if (file.exists(paste(r, "/graphs/graph", i, ".gml", sep="")) == TRUE) {
+        print(paste("Read graph: ", r, "/graphs/graph", i, ".gml", sep=""))
+        simEndGraph = paste(r, "/graphs/graph", i, ".gml", sep="")
+        graph = read.graph(simEndGraph, format="graphml")
+        delay <- get.vertex.attribute(graph, "average_delay_in_queue");
+        dflocal$delay[(1 + (counter - 1) * nodes):(nodes * counter)] <- delay
+        counter <- counter + 1
+      }
+    }
+
+    ## calc mean and standard error
+    delay.se <- sd(dflocal$delay) / sqrt(length(dflocal$delay))
+    df$meanDelay[i] <- mean(dflocal$delay)
+    df$ucl[i] <- df$meanDelay[i] + 1.96 * delay.se
+    df$lcl[i] <- df$meanDelay[i] - 1.96 * delay.se
   }
-  
-  p <- ggplot(df, aes(x=time, y=averageDelay))
+
+  df$time = seq(1, graphs) * interval;
+
+  p <- ggplot(df, aes(x=time, y=meanDelay))
   p <- p + layer(geom = "line")
+  p <- p + geom_smooth(aes(min=lcl, max=ucl), stat="identity")
   p <- p + scale_y_continuous("Average Delay")
   p <- p + scale_x_continuous("Time")
   p <- p + opts(title="Evolution of Average Delay in Queue in the System")
