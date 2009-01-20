c073a3e27d837fc3c879d36126263d561308edd2
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2009-01-20 12:52:16 +0000

    Added autoconf scripts to the octave packages.

---
+++ octave/dd_queueing/inst/queueing-greeks.m
@@ -0,0 +1,135 @@
+## Copyright (C) 2009 Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+##  
+## This file is free software; as a special exception the author gives
+## unlimited permission to copy and/or distribute it, with or without 
+## modifications, as long as this notice is preserved.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+## implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+## Author: Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+## Maintainer: Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
+## Keywords: Utilisation vs. Mean total delay, greeks, Delta, Gamma
+## Created: 15.01.2009
+## Version: 0.1
+
+function f = differentiate(f1, f2, h)
+  f = (f2 - f1) / (2 * h)
+endfunction
+
+
+function greeksDelay(prefix, X, yU, yD, thetaU, thetaD, betaU, \
+		     betaD, RU, RD, step, min, max)
+  ## write the surrogate details
+  surrogateDetailDelay(prefix, X, yU, yD, thetaU, thetaD, betaU, \
+		       betaD, RU, RD, step, min, max);
+
+  ## calculate the Delta and Gamma
+  surrogate = [prefix "-surrogate-details-delay.dat"];
+  Delta = greeksDeltaUtilisationDelay(surrogate);
+  Gamma = greeksGammaUtilisation(surrogate, Delta);
+
+  ## write the output into a gnuplot friendly file
+  data = csvread(surrogate);
+  [r,c] = size(data);
+
+  outfile = [prefix "-surrogate-greeks-details-delay.dat"];
+  fd = fopen(outfile, "wt");
+  for i = (3:(r - 2))
+    fprintf(fd, "%.6f,%.6f,%.6f,%.6f\n", data(i, 1), data(i, 1), Delta(i-1), Gamma(i-2));
+  endfor
+  fclose(fd)
+endfunction
+
+
+function surrogateDetailDelay(prefix, X, yU, yD, thetaU, thetaD, betaU, \
+			      betaD, RU, RD, step, min, max)
+  samples = (max(1) - min(1)) / step;
+  x1_vec = linspace(min(1), max(1), samples);
+  x2_vec = linspace(min(2), max(2), samples);
+
+  f = ones(rows(X), 1);
+
+  outfile = [prefix "-surrogate-details-delay.dat"];
+  fd = fopen(outfile, "wt");
+  for x1 = x1_vec
+    for x2 = x2_vec
+      krigU = krig([x1, x2], X, RU, betaU, thetaU, yU, f)(1);
+      krigD = krig([x1, x2], X, RD, betaD, thetaD, yD, f)(1);
+      fprintf(fd, "%.6f,%.6f,%.6f,%.6f\n", x1, x2, krigU, krigD);
+    endfor
+    fprintf(fd, "\n");
+  endfor
+  fclose(fd);
+endfunction
+
+
+function surrogateDetailJobs(prefix, X, yU, yL, thetaU, thetaL, betaU, \
+			     betaL, RU, RL, step, min, max)
+  samples = (max(1) - min(1)) / step;
+  x1_vec = linspace(min(1), max(1), samples);
+  x2_vec = linspace(min(2), max(2), samples);
+
+  f = ones(rows(X), 1);
+
+  outfile = [prefix "-surrogate-details-jobs.dat"];
+  fd = fopen(outfile, "wt");
+  for x1 = x1_vec
+    for x2 = x2_vec
+      krigU = krig([x1, x2], X, RU, betaU, thetaU, yU, f)(1);
+      krigL = krig([x1, x2], X, RL, betaL, thetaL, yL, f)(1);
+      fprintf(fd, "%.6f,%.6f,%.6f,%.6f\n", x1, x2, krigU, krigL);
+    endfor
+    fprintf(fd, "\n");
+  endfor
+  fclose(fd);
+endfunction
+
+
+function Delta = greeksDeltaUtilisationDelay(surrogate)
+  data = csvread(surrogate);
+  [r,c] = size(data);
+
+  ## the array is 2 elements smaller than the number of rows in data,
+  ## because we use the central difference scheme
+  Delta = zeros(0, r - 2);
+
+  for i = (2:(r-1))
+    ## calculate the step size (it is not constant)
+    h = abs((data((i+1), 3) - data((i-1), 3)) / 2);
+    Delta(i-1) = differentiate(data((i-1), 4), data((i+1), 4), h);
+  endfor
+endfunction
+
+
+function Delta = greeksDeltaUtilisationJobs(surrogate)
+  data = csvread(surrogate);
+  [r,c] = size(data);
+
+  ## the array is 2 elements smaller than the number of rows in data,
+  ## because we use the central difference scheme
+  Delta = zeros(0, r - 2);
+
+  for i = (2:(r-1))
+    ## calculate the step size (it is not constant)
+    h = abs((data((i+1), 3) - data((i-1), 3)) / 2);
+    Delta(i-1) = differentiate(data((i-1), 4), data((i+1), 4), h);
+  endfor
+endfunction
+
+
+function Delta = greeksGammaUtilisation(surrogate, Delta)
+  data = csvread(surrogate);
+  [r,c] = size(data);
+
+  ## the array is 4 elements smaller than the number of rows in data,
+  ## because we use the second derivative using the central difference scheme
+  Gamma = zeros(0, r - 4);
+  
+  for i = (3:(r-2))
+    ## calculate the step size (it is not constant)
+    h = abs((data((i+1), 3) - data((i-1), 3)) / 2);
+    Gamma(i-2) = differentiate(Delta(i-1), Delta(i-1), h);
+  endfor
+endfunction
